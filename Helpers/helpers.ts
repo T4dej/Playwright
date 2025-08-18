7bddf73f754db1f38df784c61d54d894a47dfb53
Tadej C <44680106+T4dej@users.noreply.github.com>
2025-08-18 18:00:00 +0200

    lower timeout

--- Helpers/helpers.ts
+++ Helpers/helpers.ts
@@ -1,3 +1,3 @@
-export const randomTimeout = (min = 1000, max = 4000) => {
+export const randomTimeout = (min = 1000, max = 2000) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
